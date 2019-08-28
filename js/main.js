370c72879fb2a753e7b33e2374cb1e40f14d66b7
cleritus <lukasz.stank@gmail.com>
2019-08-28 12:23:58 +0200

    add jS hero-banner, change header, remove section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,26 @@ $('.back-btn').on('click', function () {
     scrollTop: $('body').offset().top
   })
 })
+
+// Banner-area slider
+
+const bannerArea = document.querySelector('.banner-area');
+const imageList = [
+  imgSrc = 'url("../image/quad-black.jpg")',
+  imgSrc = 'url("../image/bmw-silver.jpg")',
+  imgSrc = 'url("../image/alfa-red.jpg")',
+  imgSrc = 'url("../image/lexus-blue.jpg")',
+];
+
+let number = 0;
+
+const changeSlide = () => {
+  number++;
+  if (number === imageList.length) {
+    number = 0
+  }
+  bannerArea.style.backgroundImage = imageList[number];
+  bannerArea.style.transition = 'background-image 1s';
+}
+
+let intervalSlide = setInterval(changeSlide, 5000)
