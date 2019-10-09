fabbcc09b19cdad50866452125b742935a370a1f
cleritus <lukasz.stank@gmail.com>
2019-10-09 15:31:56 +0200

    add driving.html

--- js/main-slider.js
+++ js/main-slider.js
@@ -3,24 +3,24 @@
 const bannerArea = document.querySelector('.banner-area');
 const imageList = [
   imgSrc = 'url("image/bmw2.jpg")',
-  imgSrc = 'url("image/Buggy (10).jpg")',
-  imgSrc = 'url("image/mercedes (2).jpg")',
   imgSrc = 'url("image/Ford_ranger (14).jpg")',
+  imgSrc = 'url("image/mini_morris (2).jpg")',
+  imgSrc = 'url("image/mercedes (2).jpg")',
 ];
 
 const productName = document.querySelector('.hero-banner-product-name');
 const nameList = [
   'bmw x6 240V',
-  'buggy 4x4 dwuosobowy',
+  'ford ranger monster track 4x4',
+  'Mini Morris Cooper',
   'mercedes dwuosobowy',
-  'ford ranger monster track 4x4'
 ];
 const productPrice = document.querySelector('.hero-banner-price');
 const priceList = [
   '1799 pln',
-  '1499 pln',
-  '1699 pln',
-  '2149 pln'
+  '2149 pln',
+  '799 pln',
+  '1699 pln'
 ];
 
 let number = 0;
@@ -37,26 +37,3 @@ const changeSlide = () => {
 }
 
 let intervalSlide = setInterval(changeSlide, 5000);
-
-// // Open and close modal functions
-
-// const openerList = document.querySelectorAll('.product-image');
-// const modal = document.querySelector('.modal-container');
-// const modalBtn = document.querySelector('.closeBtn')
-
-// const startModal = () => {
-//   modal.style.display = "flex";
-// console.log('ok');
-//   document.querySelector('.test').textContent = 'ok';
-// }
-
-// //  Close modal by 'escape' key on keyboard
-// const closeModal = (e) => {
-//   if (e.keyCode === 27) modal.style.display = "none";
-// }
-
-// // close by button
-// openerList.forEach(openerItem => openerItem.addEventListener('click', startModal));
-
-// document.body.addEventListener('keydown', closeModal);
-// // modalBtn.addEventListener('click', () => modal.style.display = "none");
